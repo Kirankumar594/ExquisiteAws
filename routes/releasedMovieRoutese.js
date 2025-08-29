43a6e27c11b2c9ab3518d26f3d513679ea101498
Parnets19 <parnetstech19@gmail.com>
2025-08-29 17:41:23 +0530

    deploye

--- routes/releasedMovieRoutese.js
+++ routes/releasedMovieRoutese.js
@@ -7,14 +7,20 @@ import {
   deleteReleasedMovie,
 } from '../controllers/releasedMovieControllers.js';
 import multer from 'multer';
-import { uploadReleasedMovie } from '../middleware/RehdVodeo.js';
-// const uploadReleasedMovie=multer();
+// import { uploadReleasedMovie } from '../middleware/RehdVodeo.js';
+const uploadReleasedMovie=multer();
 const router = express.Router();
 
-router.post('/', uploadReleasedMovie, createReleasedMovie);
+router.post('/', uploadReleasedMovie.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'video', maxCount: 1 },
+  ]), createReleasedMovie);
 router.get('/', getAllReleasedMovies);
 router.get('/:id', getReleasedMovieById);
-router.put('/:id', uploadReleasedMovie, updateReleasedMovie);
+router.put('/:id', uploadReleasedMovie.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'video', maxCount: 1 },
+  ]), updateReleasedMovie);
 router.delete('/:id', deleteReleasedMovie);
 
 export default router;
